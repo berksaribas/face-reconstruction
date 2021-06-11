c537a4c62f798e216a2fa48fe75593c547bb7d92
Berk Emre Saribas <berksaribas@sabanciuniv.edu>
2021-06-11 11:25:59 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+
+#include "Eigen.h"
+#include <opencv2/opencv.hpp>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_processing/render_face_detections.h>
+#include <dlib/image_processing.h>
+
+int main() {
+	return 0;
+}
