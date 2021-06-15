0e5e291ee30571329df1d7056ba427c98c067316
Philip <philngbr@googlemail.com>
2021-06-15 23:26:05 +0200

    Added code for landmark extraction from rgb images

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,24 @@
 #include <fstream>
 
 #include "Eigen.h"
-#include <opencv2/opencv.hpp>
-#include <dlib/image_processing/frontal_face_detector.h>
-#include <dlib/image_processing/render_face_detections.h>
-#include <dlib/image_processing.h>
+//#include <opencv2/opencv.hpp>
 
-int main() {
-	return 0;
+#include <opencv2/highgui/highgui.hpp>
+#include <dlib/gui_widgets.h>
+
+#include "FacialLandmarkDetection.h";
+
+#define DETECT_FACIAL_LANDMARKS		1
+#define SHOW_FACIAL_LANDMARKS		1
+#define sourcePath					"C:/Users/Philip/Desktop/Github/Uni/MS2-Git/3D_Scanning/face-reconstruction/data/Face3/realsense_face3_sf2_deep.png"
+
+using namespace dlib;
+using namespace std;
+
+int main(int argc, char** argv) {
+	//return 0;
+	if (DETECT_FACIAL_LANDMARKS) {
+		std::vector<full_object_detection> detectedLandmarks;
+		detectedLandmarks = DetectLandmarks(sourcePath, SHOW_FACIAL_LANDMARKS, true);
+	}
 }
